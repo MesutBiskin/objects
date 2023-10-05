d17c68e3d1c8354a2b73bf151c171d827cf1b25a
Mesut Biskin <msbiskin@gmail.com>
2023-10-05 16:54:38 -0400

    First objects commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const person = {
+    firstName: "Todd",
+    age: 28,
+}
+
+console.log(person.firstName) // print the person name
+
+// add one to the person`s age
+person.age++
+
+console.log(person.age)
+
+// add last name
+person.lastName = "ALbert"
+
+console.log(person.lastName)
+/*
+const person = {
+    firstName: "Todd",
+    age: 29,
+    lastName: "ALbert"
+}
+*/
+console.log(person)
